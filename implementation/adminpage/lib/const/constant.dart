f32c96f3be68ac4728865c776cdceb4fa55ee11a
yesAlima <113106857+yesAlima@users.noreply.github.com>
2024-05-09 12:52:13 +0300

    Removed adminpage folder
    removed adminpage folder because everything now exists in senior

--- implementation/adminpage/lib/const/constant.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-const cardBackgroundColor = Color.fromARGB(255, 59, 212, 115);
-const primaryColor = Color(0xFF2697FF);
-const secondaryColor = Color(0xFFFFFFFF);
-const backgroundColor = Color.fromARGB(255, 255, 255, 255);
-const selectionColor = Color(0xFF88B2AC);
-
-const defaultPadding = 16.0;
