dd7bf8ceb952a87fcb923ac21e24c737535d45df
Ahmed2309 <ahmedmh23009@gmail.com>
2024-05-10 17:52:52 +0300

    calendar edition

--- implementation/senior/lib/admin/appointment.dart
+++ implementation/senior/lib/admin/appointment.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AppointmentCalender extends StatelessWidget {
   const AppointmentCalender({Key? key});
@@ -15,65 +15,90 @@ class AppointmentCalender extends StatelessWidget {
             // StreamBuilder to listen for changes in Firestore collection
             stream: FirebaseFirestore.instance.collection('appointments').snapshots(),
             builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final List<Appointment> appointments = snapshot.data!.docs.map((doc) {
-                  final data = doc.data() as Map<String, dynamic>;
-                  final DateTime date = data['date'].toDate();
-                  final int hour = data['hour'];
-                  final DateTime startTime = DateTime(date.year, date.month, date.day, hour);
-                  final DateTime endTime = startTime.add(Duration(hours: 1)); // Duration is 1 hour
-                  
-                  // Fetch dentist's name
-                  final String dentistId = data['did'];
-                  String dentistName = 'Unknown Dentist';
-                  FirebaseFirestore.instance.collection('dentist').doc(dentistId).get().then((dentistDoc) {
-                    if (dentistDoc.exists) {
-                      dentistName = dentistDoc.data()?['FullName'] ?? 'Unknown Dentist';
-                    } else {
-                      // If did doesn't exist in dentist collection, fetch from user collection
-                      FirebaseFirestore.instance.collection('user').doc(dentistId).get().then((userDoc) {
-                        if (userDoc.exists) {
-                          dentistName = userDoc.data()?['FullName'] ?? 'Unknown Dentist';
-                        }
-                      });
-                    }
-                  });
-
-                  // Fetch patient's name
-                  final String patientId = data['uid'];
-                  String patientName = 'Unknown Patient';
-                  FirebaseFirestore.instance.collection('patient').doc(patientId).get().then((patientDoc) {
-                    if (patientDoc.exists) {
-                      patientName = patientDoc.data()?['FullName'] ?? 'Unknown Patient';
-                    }
-                  });
-
-                  return Appointment(
-                    startTime: startTime,
-                    endTime: endTime,
-                    subject: 'Appointment with $patientName, Dentist: $dentistName',
-                    color: Colors.blue, // You can set colors based on different appointments
-                  );
-                }).toList();
-                return SfCalendar(
-                  view: CalendarView.timelineWeek,
-                  timeSlotViewSettings: TimeSlotViewSettings(
-                    startHour: 9, // Adjust the starting hour
-                    endHour: 18, // Adjust the ending hour
-                    timeIntervalWidth: 100, // Adjust the width of each time slot
-                  ),
-                  dataSource: AppointmentDataSource(appointments),
-                );
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else {
+              if (snapshot.connectionState == ConnectionState.waiting) {
                 return Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              }
+              if (snapshot.hasData) {
+                return FutureBuilder<List<Appointment>>(
+                  future: _fetchAppointments(snapshot.data!.docs),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    }
+                    final List<Appointment> appointments = snapshot.data!;
+                    return SfCalendar(
+                      view: CalendarView.timelineWeek,
+                      timeSlotViewSettings: TimeSlotViewSettings(
+                        startHour: 9, // Adjust the starting hour
+                        endHour: 18, // Adjust the ending hour
+                        timeIntervalWidth: 100, // Adjust the width of each time slot
+                      ),
+                      dataSource: AppointmentDataSource(appointments),
+                    );
+                  },
+                );
+              } else {
+                return Center(child: Text('No data available'));
               }
             },
           ),
         ),
       ),
     );
+  }
+
+ 
+  Future<List<Appointment>> _fetchAppointments(List<DocumentSnapshot> documents) async {
+    final List<Appointment> todayAppointments = [];
+    final now = DateTime.now();
+
+    for (final doc in documents) {
+      final data = doc.data() as Map<String, dynamic>;
+      final DateTime date = data['date'].toDate();
+
+      if (date.year == now.year && date.month == now.month && date.day == now.day) {
+        final int hour = data['hour'];
+        final DateTime startTime = DateTime(date.year, date.month, date.day, hour);
+        final DateTime endTime = startTime.add(Duration(hours: 1));
+
+        final String patientId = data['uid'];
+        final String patientName = await getPatientName(patientId);
+        
+
+        final colorSnapshot = await FirebaseFirestore.instance.collection('color').doc(patientId).get();
+        final String? color = colorSnapshot.data()?['color'];
+
+        todayAppointments.add(Appointment(
+          startTime: startTime,
+          endTime: endTime,
+          subject: 'Appointment with $patientName',
+          color: color != null ? Color(int.parse(color)) : Colors.blue,
+        ));
+      }
+    }
+
+    return todayAppointments;
+  }
+
+  Future<String> getPatientName(String patientId) async {
+    String patientName = 'Unknown Patient';
+    final patientDoc = await FirebaseFirestore.instance.collection('user').doc(patientId).get();
+    if (patientDoc.exists) {
+      final fullName = patientDoc.data()?['FullName'] ?? 'Unknown Patient';
+      final List<String> names = fullName.split(' ');
+      if (names.length >= 2) {
+        patientName = '${names[0]} ${names[1]}';
+      } else {
+        patientName = fullName;
+      }
+    }
+    return patientName;
   }
 }
 
