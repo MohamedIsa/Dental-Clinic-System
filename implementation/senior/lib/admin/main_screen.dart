d235151f28c6a84c37f4d146d7fe6669096b9ed7
MohamedIsa <gogetassj4ul4@gmail.com>
2024-05-16 17:11:06 +0300

    update files

--- implementation/senior/lib/admin/main_screen.dart
+++ implementation/senior/lib/admin/main_screen.dart
@@ -84,6 +84,9 @@ class MainScreen extends StatelessWidget {
                   ),
                   onPressed: () {
                     FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamed(
+                      '/login'
+                    );
                   },
                 ),
               ],
